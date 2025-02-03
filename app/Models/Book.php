f3c3330fb8d6e1731b10dd0e70958bb4329c32b0
Pawel Kawa <ramzord@gmail.com>
2025-02-03 23:00:04 +0000

    Created fake data to populate db

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
+	use HasFactory;
 	public function reviews()
 	{
 		return $this->hasMany(Review::class);
--- database/migrations/2025_01_27_231220_create_reviews_table.php
+++ database/migrations/2025_01_27_231220_create_reviews_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
 	{
 		Schema::create('reviews', function (Blueprint $table) {
 			$table->id();
+			$table->foreignId('book_id')->constrained()
+				->cascadeOnDelete();
 			$table->text('review');
 			$table->unsignedTinyInteger('rating');
 			$table->timestamps();
 			// $table->foreign('book_id')->references('id')->on('books')
 			//     ->onDelete('cascade');
-			$table->foreignId('book_id')->constrained()
-				->cascadeOnDelete();
 		});
 	}
 
