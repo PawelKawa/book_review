54c16b17c11b86fc1652ceef7cf01231d2ab6945
Pawel Kawa <ramzord@gmail.com>
2025-02-05 22:43:09 +0000

    Added first local query, tested in tinker

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -11,5 +12,9 @@ class Book extends Model
 	public function reviews()
 	{
 		return $this->hasMany(Review::class);
+	}
+	public function scopeTitle(EloquentBuilder $query, string $title): EloquentBuilder
+	{
+		return $query->where('title', 'LIKE', '%' . $title . '%');
 	}
 }
