a2739677733be667ad3d3558407530413124804a
Pawel Kawa <ramzord@gmail.com>
2025-02-09 11:27:00 +0000

    Added another functions filters into Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -17,13 +17,17 @@ class Book extends Model
 	{
 		return $query->where('title', 'LIKE', '%' . $title . '%');
 	}
-	public function scopePopular(EloquentBuilder $query): EloquentBuilder
+	public function scopePopular(EloquentBuilder $query, $from = null, $to = null): EloquentBuilder
 	{
-		return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+		return $query->withCount([
+			'reviews' => fn(EloquentBuilder $q) => $this->dateRangeFilter($q, $from, $to)
+		])->orderBy('reviews_count', 'desc');
 	}
-	public function scopeHighestRated(EloquentBuilder $query): EloquentBuilder
+	public function scopeHighestRated(EloquentBuilder $query, $from = null, $to = null): EloquentBuilder
 	{
-		return $query->withAvg('reviews', 'rating')
+		return $query->withAvg([
+			'reviews' => fn(EloquentBuilder $q) => $this->dateRangeFilter($q, $from, $to)
+		], 'rating')
 		->withCount('reviews')
 		->orderBy('reviews_avg_rating', 'desc');
 	}
@@ -33,5 +37,21 @@ class Book extends Model
 		->withCount('reviews')
 		->orderBy('reviews_avg_rating', 'desc')
 		->orderBy('reviews_count', 'desc');
+	}
+	public function scopeMinReviews(EloquentBuilder $query, int $minReviews): EloquentBuilder
+	{
+		return $query->withCount('reviews')
+		->having('reviews_count', '>=', $minReviews);
+	}
+	private function dateRangeFilter(EloquentBuilder $query, $from = null, $to = null): EloquentBuilder
+	{
+		if ($from && $to) {
+			return $query->whereBetween('created_at', [$from, $to]);
+		} elseif ($from) {
+			return $query->where('created_at', '>=', $from);
+		} elseif ($to) {
+			return $query->where('created_at', '<=', $to);
+		}
+		return $query;
 	}
 }
