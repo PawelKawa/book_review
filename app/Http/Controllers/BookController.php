6913c7a508e5ebd3372b1b69e6e36beeb28b09e3
Pawel Kawa <ramzord@gmail.com>
2025-02-18 21:36:13 +0000

    single book view added

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -53,9 +53,16 @@ class BookController extends Controller
 	/**
 	 * Display the specified resource.
 	 */
-	public function show(string $id)
+	public function show(Book $book)
 	{
-		//
+
+		return view('books.show', [
+			'book' => $book->load([
+				'reviews' => fn($query) => $query->latest()
+			]),
+			'averageRating' => $book->reviews->avg('rating'),
+			'numberOfReviews' => $book->reviews->count()
+		]);
 	}
 
 	/**
